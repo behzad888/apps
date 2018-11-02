c60c39c42ee47f2378b564c4d85fa8843422d20a
Luke Schoen <ltfschoen@gmail.com>
2018-11-02 13:33:31 +0100

    refactor: Move observableAll into KeyringObservable.all

--- packages/ui-keyring/src/observable/index.ts
+++ packages/ui-keyring/src/observable/index.ts
@@ -2,18 +2,44 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
-import { combineLatest } from 'rxjs';
+import { combineLatest, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import accounts from './accounts';
 import addresses from './addresses';
 
-export default combineLatest(
-  accounts.subject,
-  addresses.subject
-).pipe(
-  map(([accounts, addresses]) => ({
-    accounts,
-    addresses
-  }))
-);
+class KeyringObservable {
+  private _all: Observable<any>;
+
+  constructor () {
+    this._all = combineLatest(
+      accounts.subject,
+      addresses.subject
+    ).pipe(
+      map(([accounts, addresses]) => ({
+        accounts,
+        addresses
+      }))
+    );
+  }
+
+  get all () {
+    return this._all;
+  }
+
+  observableAll (): Observable<any> {
+    return combineLatest(
+      accounts.subject,
+      addresses.subject
+    ).pipe(
+      map(([accounts, addresses]) => ({
+        accounts,
+        addresses
+      }))
+    );
+  }
+}
+
+const keyringObservableInstance = new KeyringObservable();
+
+export default keyringObservableInstance;
